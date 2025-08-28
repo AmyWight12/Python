0a567f479f7d9f1249e72f3966a6d00be6e34b41
Amy <michaela.wightt@gmail.com>
2025-08-28 10:25:58 +0200

    Delete Module 3 directory

--- Module 3/boolean-operators-lab.py
+++
@@ -1,6 +0,0 @@
-#Scenario: Using one of the comparison operators in Python, write a simple two-line program that takes the parameter n 
-#as input, which is an integer, and prints False if n is less than 100, and True if n is greater than or equal to 100.
-#Don't create any if blocks
-
-n = int(input("enter a number: "))
-print(n >= 100)
