04e0481fc7f0962be284aaa492b8f5b4a1860032
Amy <michaela.wightt@gmail.com>
2025-08-28 10:25:44 +0200

    Delete Module 2 directory

--- Module 2/input-func-lab-expressions1.py
+++
@@ -1,7 +0,0 @@
-#Your task is to complete the code in order to evaluate the following expression:
-#The result should be assigned to y.
-
-x = float(input("Enter value for x: "))
-
-y = 1 / (x + (1/(x + (1/(x + (1/(x + (1/x))))))))
-print("y =", y)
